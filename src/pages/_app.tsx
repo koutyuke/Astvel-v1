86455648ab2d26a658a53b972a4ad65f432e27d3
koutyuke <gunji.kousuke08@gmail.com>
2023-02-22 17:32:35 +0900

    add/destyle.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "destyle.css"
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
