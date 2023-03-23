e3ba6ba5630571e07d4a90d480cfd7b99b159eb2
koutyuke <gunji.kousuke08@gmail.com>
2023-03-23 11:37:52 +0900

    🌟(create)/Member Model(small)を作成

---
+++ src/components/models/member/smallMember.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+import * as Avatar from "@radix-ui/react-avatar";
+
+type Props = {
+  imageUrl: string;
+  name: string;
+} & Omit<ComponentPropsWithoutRef<"div">, "children">;
+
+const SmallMemberModel: FC<Props> = ({ imageUrl, name, className, ...props }) => (
+  <Avatar.Root
+    className={`${className} flex h-[30px] items-center justify-start space-x-1 bg-gray-300 pl-1`}
+    {...props}
+  >
+    <Avatar.Image src={imageUrl} className="h-6 w-6 rounded-full" />
+    <p className="truncate">
+      <span className="text-sm">{name}</span>
+    </p>
+  </Avatar.Root>
+);
+
+export default SmallMemberModel;
