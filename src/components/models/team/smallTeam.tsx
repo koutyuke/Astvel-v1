80464f7a546a5235ef9db4fb63c781635f4d29e0
koutyuke <gunji.kousuke08@gmail.com>
2023-03-23 11:38:21 +0900

    🌟(create)/Team Model(small)を作成

---
+++ src/components/models/team/smallTeam.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+
+type Props = {
+  emoji: string;
+  name: string;
+} & Omit<ComponentPropsWithoutRef<"div">, "children">;
+
+const SmallTeamModel: FC<Props> = ({ emoji, name, className, ...props }) => (
+  <div className={`${className} flex h-[30px] items-center justify-start space-x-1 pl-1`} {...props}>
+    <div className="w-6 text-2xl">{emoji}</div>
+    <p className="truncate">
+      <span className="text-sm">{name}</span>
+    </p>
+  </div>
+);
+
+export default SmallTeamModel;
