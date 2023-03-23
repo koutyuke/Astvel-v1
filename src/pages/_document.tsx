8b9c591632ff3e5668bb029b59e58f3e41e05ab1
koutyuke <gunji.kousuke08@gmail.com>
2023-03-23 11:41:12 +0900

    ➕(add)/draggable属性の無効化

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html lang="ja">
     <Head />
-    <body className="bg-gray-900">
+    <body className="bg-gray-900" draggable={false}>
       <Main />
       <NextScript />
     </body>
