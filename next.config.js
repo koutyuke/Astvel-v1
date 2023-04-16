e6c0ebd627a292b50f863e9bcca7f0623d5b897a
koutyuke <gunji.kousuke08@gmail.com>
2023-04-16 19:28:19 +0900

    📕(add)/dnd-kitの追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withInterceptStdout = require('next-intercept-stdout');
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+const nextConfig = withInterceptStdout(
+  {
+    reactStrictMode: true,
+    swcMinify: true,
+  },
+  (text) => (text.includes('Duplicate atom key') ? '' : text)
+);
 
 module.exports = nextConfig
