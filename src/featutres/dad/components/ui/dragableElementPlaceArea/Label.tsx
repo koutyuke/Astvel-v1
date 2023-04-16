c59d39aa4bb355211b2ce3a87a4b84aa640c97a5
koutyuke <gunji.kousuke08@gmail.com>
2023-04-16 19:34:39 +0900

    ️⭐️(create)/drop areaの実装

---
+++ src/featutres/dad/components/ui/dragableElementPlaceArea/Label.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react";
+import { TbTriangleInvertedFilled } from "react-icons/tb";
+
+type Props = {
+  label: string;
+  children: ReactNode;
+};
+
+const LabelWrapper: FC<Props> = ({ label, children }) => (
+  <div className="flex w-full flex-col space-y-1 p-1">
+    <div className="flex w-full items-center justify-start space-x-1">
+      <TbTriangleInvertedFilled size={10} color="#9c9cad" />
+      <p className="text-sm text-gray-400">{label}</p>
+    </div>
+    {children}
+  </div>
+);
+
+export default LabelWrapper;
---
+++ src/featutres/dad/components/ui/dragableElementPlaceArea/index.tsx
@@ -0,0 +1,46 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+import { GroupType } from "types/models/dnd";
+import { Member, Team } from "types/models/data";
+import LargeDragMember from "../../models/member/large";
+import LargeDragTeam from "../../models/team/large";
+import LabelWrapper from "./Label";
+
+type Props = {
+  group: GroupType;
+  members: Member[];
+  teams?: Team[];
+} & ComponentPropsWithoutRef<"div">;
+
+const DraggableElememntPlaceArea: FC<Props> = ({ group, members, className, teams, ...others }) => {
+  const isMembersShow = members.filter(member => member.isShow).length;
+  const isTeamsShow = teams === undefined ? 0 : teams.filter(team => team.isShow).length;
+
+  return (
+    <div className={`${className} space-y-1 rounded-md p-2 text-black `} {...others}>
+      {isMembersShow !== 0 && (
+        <LabelWrapper label="Member">
+          <div className="pl-2">
+            {members.map(member => (
+              <LargeDragMember member={member} group={group} key={member.id} />
+            ))}
+          </div>
+        </LabelWrapper>
+      )}
+      {isTeamsShow !== 0 && (
+        <LabelWrapper label="Team">
+          <div className="grid w-full grid-cols-[repeat(auto-fit,5rem)] grid-rows-[repeat(auto-fit,6rem)] pl-2">
+            {teams?.map(team => (
+              <LargeDragTeam team={team} group={group} key={team.id} />
+            ))}
+          </div>
+        </LabelWrapper>
+      )}
+    </div>
+  );
+};
+
+DraggableElememntPlaceArea.defaultProps = {
+  teams: undefined,
+};
+
+export default DraggableElememntPlaceArea;
