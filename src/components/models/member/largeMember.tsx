fa0ed13a2094b8be29dc605079f6573925e0d1bf
koutyuke <gunji.kousuke08@gmail.com>
2023-04-17 09:17:56 +0900

    🔨(fix)/アルファベットの下部がhiddenで消される問題を修正

--- src/components/models/member/largeMember.tsx
+++ src/components/models/member/largeMember.tsx
@@ -10,7 +10,7 @@ const LargeMemberModel = forwardRef<HTMLDivElement, Props>(({ imageUrl, name, cl
   <div className={`${className} h-24 w-20 rounded-xl`} ref={ref} style={style} {...other}>
     <Avatar.Root className="flex h-24 w-20 flex-col items-center justify-center space-y-1 p-[2px]">
       <Avatar.Image src={imageUrl} className="h-14 w-14 rounded-full" />
-      <p className="h-4 w-[4.5rem] overflow-hidden text-ellipsis text-center">
+      <p className="h-6 w-[4.5rem] overflow-hidden text-ellipsis text-center">
         <span className="text-xs">{name}</span>
       </p>
     </Avatar.Root>
