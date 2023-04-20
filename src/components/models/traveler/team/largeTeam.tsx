140cf91df83cd92091cb44db9f1bed976a99d3b3
koutyuke <gunji.kousuke08@gmail.com>
2023-04-21 01:19:08 +0900

    🔨(fix)/余分なクラスの削除

--- src/components/models/traveler/team/largeTeam.tsx
+++ src/components/models/traveler/team/largeTeam.tsx
@@ -12,7 +12,7 @@ const LargeTeamModel = forwardRef<HTMLDivElement, Props>(({ emoji, name, classNa
     {...other}
   >
     <div className="flex h-14 w-14 items-center justify-center rounded-lg bg-white">
-      <p className="text-5xl text-black">{emoji === "" ? name[0] : emoji}</p>
+      <p className="text-5xl">{emoji === "" ? name[0] : emoji}</p>
     </div>
     <p className="h-6 w-[4.5rem] overflow-hidden text-ellipsis text-center">
       <span className="text-xs">{name}</span>
