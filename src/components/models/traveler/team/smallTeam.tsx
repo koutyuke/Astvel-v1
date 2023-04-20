8b5eadfa995ae9fb7c18248c7fe3d0213572125d
koutyuke <gunji.kousuke08@gmail.com>
2023-04-21 01:19:18 +0900

    🔨(fix)/余分なクラスの削除

--- src/components/models/traveler/team/smallTeam.tsx
+++ src/components/models/traveler/team/smallTeam.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SmallTeamModel: FC<Props> = ({ emoji, name, className, ...props }) => (
   <div className={`${className} flex h-[30px] items-center justify-start space-x-2 pl-1`} {...props}>
-    <div className="w-6 text-2xl">{emoji}</div>
+    <div className="w-6 text-center text-2xl">{emoji === "" ? name[0] : emoji}</div>
     <p className="truncate">
       <span className="text-sm">{name}</span>
     </p>
