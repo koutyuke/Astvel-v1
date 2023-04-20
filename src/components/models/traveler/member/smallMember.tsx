8fa5f86dac261a39d078daa3b59b39ceb1771c3e
koutyuke <gunji.kousuke08@gmail.com>
2023-04-21 01:18:53 +0900

    🔨(fix)/format

--- src/components/models/traveler/member/smallMember.tsx
+++ src/components/models/traveler/member/smallMember.tsx
@@ -7,10 +7,7 @@ type Props = {
 } & Omit<ComponentPropsWithRef<"div">, "children">;
 
 const SmallMemberModel: FC<Props> = ({ imageUrl, name, className, ...props }) => (
-  <Avatar.Root
-    className={`${className} flex h-[30px] items-center justify-start space-x-2 bg-gray-300 pl-1`}
-    {...props}
-  >
+  <Avatar.Root className={`${className} flex h-[30px] items-center justify-start space-x-2 pl-1`} {...props}>
     <Avatar.Image src={imageUrl} className="h-6 w-6 rounded-full" />
     <p className="truncate">
       <span className="text-sm">{name}</span>
