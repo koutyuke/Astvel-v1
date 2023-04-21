10f14af245fe944e9bfadeb1839fb7d187f28e09
koutyuke <gunji.kousuke08@gmail.com>
2023-04-21 11:26:39 +0900

    ️⭐️(create)/categoryAccordionの実装

---
+++ src/featutres/dad/components/ui/dropableArea/destinationChannel/categoryAccordion.tsx
@@ -0,0 +1,29 @@
+import { type FC, type ReactNode } from "react";
+import * as Accordion from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { BiCategory } from "react-icons/bi";
+import { IoLockClosed } from "react-icons/io5";
+
+type Props = {
+  children: ReactNode;
+  name: string;
+  value: string;
+  isPrivate: boolean;
+};
+
+const CategoryAccordion: FC<Props> = ({ children, name, value, isPrivate }) => (
+  <Accordion.Item value={value} className="w-full">
+    <Accordion.Header>
+      <Accordion.Trigger className="group flex w-full space-x-1 p-1">
+        {isPrivate ? <IoLockClosed size={20} /> : <BiCategory size={20} />}
+        <p className=" w-full">
+          <span className="">{name}</span>
+        </p>
+        <ChevronDownIcon className="h-4 w-4 duration-300 group-data-[state=closed]:rotate-180" />
+      </Accordion.Trigger>
+    </Accordion.Header>
+    <Accordion.Content>{children}</Accordion.Content>
+  </Accordion.Item>
+);
+
+export default CategoryAccordion;
