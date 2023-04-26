8d76b811e440a5e2097f4f7f4862379750f8b4d2
koutyuke <gunji.kousuke08@gmail.com>
2023-04-26 14:16:00 +0900

    🔨(fix)/カラーコードtailwindに揃える

--- src/featutres/dad/components/ui/dragableElementPlaceArea/Label.tsx
+++ src/featutres/dad/components/ui/dragableElementPlaceArea/Label.tsx
@@ -9,7 +9,7 @@ type Props = {
 const LabelWrapper: FC<Props> = ({ label, children }) => (
   <div className="flex w-full flex-col space-y-1">
     <div className="flex w-full items-center justify-start space-x-1">
-      <TbTriangleInvertedFilled size={10} color="#9c9cad" />
+      <TbTriangleInvertedFilled size={10} color="#6B7280" />
       <p className="text-sm text-gray-500">{label}</p>
     </div>
     {children}
