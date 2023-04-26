7c5d6c76db79665b3c1347fa9e41329aca70875f
koutyuke <gunji.kousuke08@gmail.com>
2023-04-26 14:04:02 +0900

    🔨(fix)/headerのリンクを修正

--- src/components/ui/layouts/header/HeaderLinks.tsx
+++ src/components/ui/layouts/header/HeaderLinks.tsx
@@ -1,19 +1,11 @@
 import InternalLink from "components/elements/button/InternalLink";
 
 const HeaderLinks = () => (
-  <div className="flex h-full w-full items-center justify-center space-x-8">
-    <InternalLink url="/" className="text-xl text-white">
-      Home
-    </InternalLink>
-    <InternalLink url="how-to-use" className="text-xl text-white">
-      How-To-Use
-    </InternalLink>
-    <InternalLink url="q&a" className="text-xl text-white">
-      Q-&-A
-    </InternalLink>
-    <InternalLink url="Information" className="text-xl text-white">
-      Information
-    </InternalLink>
+  <div className="flex h-full w-full items-center justify-center space-x-8 text-xl text-white">
+    <InternalLink url="/">Home</InternalLink>
+    <InternalLink url="/guild">Guild</InternalLink>
+    <InternalLink url="/how-to-use">How-To-Use</InternalLink>
+    <InternalLink url="/q&a">Q-&-A</InternalLink>
   </div>
 );
 
