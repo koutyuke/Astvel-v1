02162e7edac729744fe0c2c9f46ce2219ac22c65
koutyuke <gunji.kousuke08@gmail.com>
2023-04-26 14:36:22 +0900

    🔨(fix)/z-indexの追加

--- src/components/ui/layouts/header/index.tsx
+++ src/components/ui/layouts/header/index.tsx
@@ -4,7 +4,7 @@ import SingInButton from "components/elements/button/singInOut";
 import HeaderLinks from "./HeaderLinks";
 
 const Header: FC = () => (
-  <header className="fixed grid h-24 w-full grid-cols-[20%_60%_20%] bg-gray-800 py-4 px-6">
+  <header className="fixed z-50 grid h-24 w-full grid-cols-[20%_60%_20%] bg-gray-800 py-4 px-6">
     <div className="flex h-full w-full items-center justify-center">
       <Image src="/logo.PNG" alt="logo" width={200} height={50} />
     </div>
