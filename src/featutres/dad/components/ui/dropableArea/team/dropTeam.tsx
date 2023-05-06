8150f33f0c6da46c0bd3dc54a4accd8dfcd96816
koutyuke <gunji.kousuke08@gmail.com>
2023-05-06 16:13:32 +0900

    🔨(fix)/teamをdragした時のdrop先のteamのhoverを変更

--- src/featutres/dad/components/ui/dropableArea/team/dropTeam.tsx
+++ src/featutres/dad/components/ui/dropableArea/team/dropTeam.tsx
@@ -1,9 +1,11 @@
 import { useDroppable } from "@dnd-kit/core";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { useState, type FC } from "react";
-import { Team } from "types/models/data";
 import { DragDataType, GroupTeamType } from "types/models/dnd";
 import { HiUserGroup } from "react-icons/hi";
+import { Team } from "types/models/group";
+import { memberListSelector } from "utils/recoil/keys";
+import { useRecoilValue } from "recoil";
 import DraggableElememntPlaceArea from "../../dragableElementPlaceArea";
 import ViewTraveler from "../../viewTraveler";
 
@@ -13,7 +15,7 @@ type Props = {
 };
 
 const DropTeam: FC<Props> = ({ group, team }) => {
-  const { id, name, members, iconEmoji } = team;
+  const { id, name, members: memberIdList, iconEmoji } = team;
   const { isOver, active, setNodeRef } = useDroppable({
     id,
     data: {
@@ -23,11 +25,16 @@ const DropTeam: FC<Props> = ({ group, team }) => {
   });
   const [isOpen, setOpen] = useState<boolean>(false);
   const activeGroup = active?.data.current as DragDataType | undefined;
-  const isKeep = !!(activeGroup !== undefined && activeGroup.group.type === "team" && activeGroup.group.teamId === id);
+  const isDragTeam = activeGroup?.dataType === "team";
+  const members = useRecoilValue(memberListSelector(memberIdList));
+
+  if (members === undefined) {
+    return <div>error</div>;
+  }
 
   return (
     <div
-      className={`${isOver ? "scale-[1.015] drop-shadow-lg" : ""}
+      className={`${isOver && !isDragTeam ? "scale-[1.015] drop-shadow-lg" : ""}
         flex w-full flex-col items-center justify-center space-y-1 rounded-lg
         bg-[rgb(255,255,255,0.6)]
         px-2 py-1 outline-2 outline-offset-[3px] outline-white duration-300
@@ -48,14 +55,10 @@ const DropTeam: FC<Props> = ({ group, team }) => {
             <span className="opacity-75">{iconEmoji}</span> {name}
           </span>
         </p>
-        <ChevronDownIcon className={`${isOpen || isKeep ? "" : "rotate-180"} h-4 w-4 duration-200`} />
+        <ChevronDownIcon className={`${isOpen ? "" : "rotate-180"} h-4 w-4 duration-200`} />
       </div>
       <div className="relative w-full">
-        {isOpen || isKeep ? (
-          <DraggableElememntPlaceArea group={group} members={members} className="" />
-        ) : (
-          <ViewTraveler members={members} />
-        )}
+        {isOpen ? <DraggableElememntPlaceArea group={group} members={members} /> : <ViewTraveler members={members} />}
       </div>
     </div>
   );
