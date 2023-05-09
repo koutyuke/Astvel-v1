f56d03b364e9976ea7e5c6401fec179941f9138c
koutyuke <gunji.kousuke08@gmail.com>
2023-05-09 15:32:08 +0900

    🔨(fix)/Tooltipを削除

--- src/components/elements/button/additional/index.tsx
+++ src/components/elements/button/additional/index.tsx
@@ -1,31 +1,24 @@
-import type { ComponentPropsWithoutRef, FC } from "react";
+import { ComponentPropsWithoutRef, ElementRef, FC, forwardRef } from "react";
 import { HiOutlinePlusSm } from "react-icons/hi";
-import * as Tooltip from "@radix-ui/react-tooltip";
 
 type Props = {
   description: string;
 } & ComponentPropsWithoutRef<"button">;
 
-const AdditionalButton: FC<Props> = ({ className, description, ...other }) => (
-  <Tooltip.Provider>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild>
-        <button
-          type="button"
-          className={`${className} flex h-10 w-10 items-center justify-center rounded-full bg-white drop-shadow-xl duration-300 hover:scale-110`}
-          {...other}
-        >
-          <HiOutlinePlusSm color="black" size={20} />
-        </button>
-      </Tooltip.Trigger>
-      <Tooltip.Portal>
-        <Tooltip.Content sideOffset={8} className="rounded-lg bg-white px-4 py-2 text-black drop-shadow-xl">
-          <p>{description}</p>
-          <Tooltip.Arrow className="fill-white" />
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip.Root>
-  </Tooltip.Provider>
+const AdditionalButton: FC<Props> = forwardRef<ElementRef<"button">, Props>(
+  ({ className, description, ...other }, ref) => (
+    <button
+      type="button"
+      className={`${className} flex h-10 w-10 items-center justify-center rounded-full bg-white drop-shadow-xl duration-300 hover:scale-110`}
+      ref={ref}
+      {...other}
+    >
+      <HiOutlinePlusSm color="black" size={20} />
+    </button>
+
+  ),
 );
+
+AdditionalButton.displayName = "AdditionalButton";
 
 export default AdditionalButton;
