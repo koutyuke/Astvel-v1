763e6d9be8ba4a263c09d2cec2726b782e33c08b
koutyuke <gunji.kousuke08@gmail.com>
2023-05-22 11:05:21 +0900

    🔨(fix)/余計なpropsを削除

--- src/components/elements/button/additional/index.tsx
+++ src/components/elements/button/additional/index.tsx
@@ -1,22 +1,18 @@
 import { ComponentPropsWithoutRef, ElementRef, FC, forwardRef } from "react";
 import { HiOutlinePlusSm } from "react-icons/hi";
 
-type Props = {
-  description: string;
-} & ComponentPropsWithoutRef<"button">;
+type Props = ComponentPropsWithoutRef<"button">;
 
-const AdditionalButton: FC<Props> = forwardRef<ElementRef<"button">, Props>(
-  ({ className, description, ...other }, ref) => (
-    <button
-      type="button"
-      className={`${className} flex h-10 w-10 items-center justify-center rounded-full bg-white drop-shadow-xl duration-300 hover:scale-110`}
-      ref={ref}
-      {...other}
-    >
-      <HiOutlinePlusSm color="black" size={20} />
-    </button>
-  ),
-);
+const AdditionalButton: FC<Props> = forwardRef<ElementRef<"button">, Props>(({ className, ...other }, ref) => (
+  <button
+    type="button"
+    className={`${className} flex h-10 w-10 items-center justify-center rounded-full bg-white drop-shadow-xl duration-300 hover:scale-110`}
+    ref={ref}
+    {...other}
+  >
+    <HiOutlinePlusSm color="black" size={20} />
+  </button>
+));
 
 AdditionalButton.displayName = "AdditionalButton";
 
--- src/components/elements/dialog/create.tsx
+++ src/components/elements/dialog/create.tsx
@@ -3,18 +3,19 @@ import * as Dialog from "@radix-ui/react-dialog";
 import AdditionalButton from "../button/additional";
 
 type Props = {
-  triggerDescription: string;
   children: ReactNode;
 };
 
-const CreateDialog: FC<Props> = ({ children, triggerDescription }) => (
+const CreateDialog: FC<Props> = ({ children }) => (
   <Dialog.Root>
     <Dialog.Trigger asChild>
-      <AdditionalButton description={triggerDescription} />
+      <AdditionalButton />
     </Dialog.Trigger>
     <Dialog.Portal className="">
       <Dialog.Overlay className="fixed inset-0 z-[51] bg-[rgba(0,0,0,0.5)]" />
-      <Dialog.Content className="fixed top-[50%] left-[50%] z-[52] translate-x-[-50%] translate-y-[-50%]">{children}</Dialog.Content>
+      <Dialog.Content className="fixed top-[50%] left-[50%] z-[52] translate-x-[-50%] translate-y-[-50%]">
+        {children}
+      </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
 );
