389356d4a43227385f1d1b33c1462e823bb22bdc
koutyuke <gunji.kousuke08@gmail.com>
2023-06-09 22:50:24 +0900

    🔨(fix)/stateによる開閉に変更

--- src/components/elements/dialog/add.tsx
+++ src/components/elements/dialog/add.tsx
@@ -1,17 +1,19 @@
-import { FC, ReactNode } from "react";
+import { Dispatch, FC, ReactNode, SetStateAction } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import AdditionalButton from "../button/additional";
 
 type Props = {
   children: ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const CreateDialog: FC<Props> = ({ children }) => (
-  <Dialog.Root>
+const DialogForAdd: FC<Props> = ({ children, open, setOpen }) => (
+  <Dialog.Root open={open} onOpenChange={setOpen}>
     <Dialog.Trigger asChild>
       <AdditionalButton />
     </Dialog.Trigger>
-    <Dialog.Portal className="">
+    <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 z-[51] bg-[rgba(0,0,0,0.5)]" />
       <Dialog.Content className="fixed top-[50%] left-[50%] z-[52] translate-x-[-50%] translate-y-[-50%]">
         {children}
@@ -20,4 +22,4 @@ const CreateDialog: FC<Props> = ({ children }) => (
   </Dialog.Root>
 );
 
-export default CreateDialog;
+export default DialogForAdd;
