1028248cef790d122f92a6c218214282a4b6f2dc
koutyuke <gunji.kousuke08@gmail.com>
2023-06-12 13:20:47 +0900

    🔨(feat)/forwardRefの追加

--- src/components/elements/button/index.tsx
+++ src/components/elements/button/index.tsx
@@ -1,13 +1,15 @@
-import { ComponentPropsWithoutRef, FC } from "react";
+import { ComponentPropsWithoutRef, FC, forwardRef } from "react";
 
 type Props = {
   children: string;
 } & ComponentPropsWithoutRef<"button">;
 
-const BaseButton: FC<Props> = ({ className, children, ...other }) => (
-  <button type="button" className={`h-10 w-20 rounded-lg text-center ${className}`} {...other}>
+const BaseButton: FC<Props> = forwardRef<HTMLButtonElement, Props>(({ className, children, ...other }, ref) => (
+  <button type="button" ref={ref} className={`h-10 w-20 rounded-lg text-center ${className}`} {...other}>
     {children}
   </button>
-);
+));
+
+BaseButton.displayName = "BaseButton";
 
 export default BaseButton;
