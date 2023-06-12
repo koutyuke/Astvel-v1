3f5e887b285b4de23ee26096e5604e52fb374f80
koutyuke <gunji.kousuke08@gmail.com>
2023-06-12 13:21:46 +0900

    🔨(feat)/HightOderComponentsにするための修正

--- src/components/elements/dialog/index.tsx
+++ src/components/elements/dialog/index.tsx
@@ -3,23 +3,23 @@ import * as Dialog from "@radix-ui/react-dialog";
 
 type Props = {
   children: ReactNode;
-  TrigerButton: FC;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const BaseDialog: FC<Props> = ({ TrigerButton, children, open, setOpen }) => (
-  <Dialog.Root open={open} onOpenChange={setOpen}>
-    <Dialog.Trigger asChild>
-      <TrigerButton />
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="fixed inset-0 z-[51] bg-[rgba(0,0,0,0.5)]" />
-      <Dialog.Content className="fixed top-[50%] left-[50%] z-[52] translate-x-[-50%] translate-y-[-50%]">
-        {children}
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+const BaseDialog: (TrigerButton: ReactNode) => FC<Props> = TrigerButton => {
+  const Di: FC<Props> = ({ children, open, setOpen }) => (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger asChild>{TrigerButton}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 z-[51] bg-[rgba(0,0,0,0.5)]" />
+        <Dialog.Content className="fixed top-[50%] left-[50%] z-[52] translate-x-[-50%] translate-y-[-50%]">
+          {children}
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+  return Di;
+};
 
 export default BaseDialog;
