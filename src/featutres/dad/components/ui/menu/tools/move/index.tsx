dfa10f703dd4a2e2503f889f9d88563ae4854c41
koutyuke <gunji.kousuke08@gmail.com>
2023-06-16 19:56:45 +0900

    ⭐(create)/Toolsの作成

---
+++ src/featutres/dad/components/ui/menu/tools/move/index.tsx
@@ -0,0 +1,19 @@
+import BaseDialog from "components/elements/dialog";
+// import useToastSetter from "hooks/useToastSetter";
+import { FC, useState } from "react";
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+import BaseToolButton from "featutres/dad/components/elements/button/tool";
+
+const ToolMoveMenu: FC = () => {
+  const [open, setOpen] = useState(false);
+  const AddDialog = BaseDialog(<BaseToolButton Icon={BsFillRocketTakeoffFill} title="Move" />);
+  // const toastSetter = useToastSetter();
+
+  return (
+    <AddDialog open={open} setOpen={setOpen}>
+      <div>jpeg</div>
+    </AddDialog>
+  );
+};
+
+export default ToolMoveMenu;
---
+++ src/featutres/dad/components/ui/menu/tools/reset/index.tsx
@@ -0,0 +1,19 @@
+import BaseDialog from "components/elements/dialog";
+// import useToastSetter from "hooks/useToastSetter";
+import { FC, useState } from "react";
+import BaseToolButton from "featutres/dad/components/elements/button/tool";
+import { TbReload } from "react-icons/tb";
+
+const ToolResetMenu: FC = () => {
+  const [open, setOpen] = useState(false);
+  const AddDialog = BaseDialog(<BaseToolButton Icon={TbReload} title="Reset" />);
+  // const toastSetter = useToastSetter();
+
+  return (
+    <AddDialog open={open} setOpen={setOpen}>
+      <div>jpeg</div>
+    </AddDialog>
+  );
+};
+
+export default ToolResetMenu;
