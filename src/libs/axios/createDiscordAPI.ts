2fd07b733c81968759866918a7077cffb23e4e86
koutyuke <gunji.kousuke08@gmail.com>
2023-06-25 22:26:58 +0900

    ⭐(create)/createDiscordAPIの作成

---
+++ src/libs/axios/createDiscordAPI.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const createDiscordAPI =(token:string) =>  axios.create({
+  baseURL: "https://discord.com/api/v10",
+  headers: { Authorization: token },
+});
+
+export default createDiscordAPI;
