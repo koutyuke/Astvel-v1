af16a02821a2de1aa02af4679adbeb478e3e552e
koutyuke <gunji.kousuke08@gmail.com>
2023-06-25 22:11:35 +0900

    ⭐(create)/api(guilds)の作成

---
+++ src/pages/api/guilds/index.ts
@@ -0,0 +1,61 @@
+import { isAxiosError } from "axios";
+import { APIGuild, APIGuildMember, Routes } from "discord.js";
+import discordAPIAccesser from "libs/axios/discordAPIAccesser";
+import prisma from "libs/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const schema = z.object({
+  guild_id: z.string(),
+  user_id: z.string(),
+});
+
+const apiGuilds = async (req: NextApiRequest, res: NextApiResponse) => {
+  const userToken = req.headers.authorization?.replace("Bearer ", "");
+  const query = schema.safeParse(req.query);
+  const botToken = process.env.DISCORD_BOT_TOKEN;
+
+  if (!query.success || userToken === undefined) {
+    return res.status(400).json({ message: "Bad Request" });
+  }
+  if (botToken === undefined) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+  const { user_id: userId, guild_id: guildId } = query.data;
+
+  try {
+
+    const [, guildData] = await Promise.all([
+      discordAPIAccesser.get<APIGuildMember>(Routes.guildMember(guildId, userId)),
+      discordAPIAccesser.get<APIGuild>(Routes.guild(guildId)).then(r => r.data),
+    ])
+
+    const account = await prisma.account.findUnique({
+      where: {
+        provider_providerAccountId: {
+          provider: "discord",
+          providerAccountId: userId,
+        },
+      },
+    });
+
+    if (account === null || account.access_token === null || account.access_token !== userToken) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+
+    return res.status(200).json({
+      id: guildData.id,
+      name: guildData.name,
+      icon: guildData.icon,
+    });
+  } catch (e) {
+    if (isAxiosError(e) && e.response) {
+      // console.log(e);
+      return res.status(e.response.status).json({ message: e.response.statusText });
+    }
+    return res.status(400).json({ message: "Bad Request" });
+  }
+};
+
+export default apiGuilds;
