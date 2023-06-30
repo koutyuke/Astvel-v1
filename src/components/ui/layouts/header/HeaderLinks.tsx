7151b5c5a312ae43438930dc94936129719520cb
koutyuke <gunji.kousuke08@gmail.com>
2023-06-30 11:59:40 +0900

    🔨(fix)/path名の変更

--- src/components/ui/layouts/header/HeaderLinks.tsx
+++ src/components/ui/layouts/header/HeaderLinks.tsx
@@ -3,7 +3,7 @@ import InternalLink from "components/elements/button/InternalLink";
 const HeaderLinks = () => (
   <div className="flex h-full w-full items-center justify-center space-x-8 text-xl text-white">
     <InternalLink url="/">Home</InternalLink>
-    <InternalLink url="/guild">Guild</InternalLink>
+    <InternalLink url="/guilds">Guilds</InternalLink>
     <InternalLink url="/how-to-use">How-To-Use</InternalLink>
     <InternalLink url="/q&a">Q-&-A</InternalLink>
   </div>
