32a9ae4f1040f919fc243a771515c86350cfacd5
koutyuke <gunji.kousuke08@gmail.com>
2023-07-02 01:18:48 +0900

    ⭐(create)/Guild選択ページの作成

---
+++ src/components/ui/selectGuild/index.tsx
@@ -0,0 +1,52 @@
+import useCurrentUserGuilds from "hooks/useCurrentUserGuilds";
+import { FC } from "react";
+import * as Tabs from "@radix-ui/react-tabs";
+import ListGuilds from "./listGuilds";
+import InviteBot from "../inviteBot.tsx";
+
+const SelectGuild: FC = () => {
+  const { data: guilds, error, isLoading } = useCurrentUserGuilds();
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  if (guilds === undefined || error) {
+    return <div>error</div>;
+  }
+
+  return (
+    <Tabs.Root
+      defaultValue="guilds"
+      className="flex h-[calc(100vh_-_10rem)] w-full  flex-col items-center justify-start text-gray-500"
+    >
+      <Tabs.TabsList className="h-16 w-full max-w-[60rem] text-center text-xl">
+        <Tabs.TabsTrigger
+          value="guilds"
+          className="h-16 w-1/2 max-w-[30rem] rounded-tl-[1rem] border-b-4 border-red-100  bg-white data-[state=active]:border-red-400"
+        >
+          Select Guilds
+        </Tabs.TabsTrigger>
+        <Tabs.TabsTrigger
+          value="bot"
+          className="h-16 w-1/2 max-w-[30rem] rounded-tr-[1rem] border-b-4 border-red-100  bg-white data-[state=active]:border-red-400"
+        >
+          Invite Bot
+        </Tabs.TabsTrigger>
+      </Tabs.TabsList>
+      <Tabs.Content
+        value="guilds"
+        className="h-[calc(100vh_-_14rem)] w-full max-w-[60rem] overflow-hidden rounded-b-[1rem] bg-white"
+      >
+        <ListGuilds />
+      </Tabs.Content>
+      <Tabs.Content
+        value="bot"
+        className="h-[calc(100vh_-_14rem)] w-full max-w-[60rem] overflow-hidden rounded-b-[1rem] bg-white"
+      >
+        <InviteBot />
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+};
+
+export default SelectGuild;
--- src/pages/guilds.tsx
+++ src/pages/guilds.tsx
@@ -14,6 +14,7 @@ import guildPageQuerySchema from "schema/guildPageQuery";
 import useGuild from "featutres/dnd/hooks/swr/useGuild";
 import useUpdateDnDTravelers from "featutres/dnd/hooks/useUpdateTravelers";
 import useUpdateDnD from "featutres/dnd/hooks/useUpdateDnD";
+import SelectGuild from "components/ui/selectGuild";
 
 const Guilds: NextPage = () => {
   const { data: SESSION } = useSession();
@@ -31,7 +32,11 @@ const Guilds: NextPage = () => {
   }
 
   if (!query.success) {
-    return <div>not query</div>;
+    return (
+      <div className="mx-10">
+        <SelectGuild />
+      </div>
+    );
   }
 
   if (guild.isLoading) {
