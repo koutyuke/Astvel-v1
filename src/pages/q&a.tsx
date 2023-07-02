0c3183d07d7ae90ae0434dea0ebc326d2f956660
koutyuke <gunji.kousuke08@gmail.com>
2023-07-02 09:07:10 +0900

    ⭐(create)/q&Aページの作成

---
+++ src/pages/q&a.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const QnA: NextPage = () => (
+  <div className="mx-10 flex h-[calc(100vh_-_10rem)] items-center justify-center text-2xl">Comming Soon....</div>
+);
+
+export default QnA;
