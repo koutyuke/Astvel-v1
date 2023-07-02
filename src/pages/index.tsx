0a02ee020a85a8e620e1a7d3b2616b41d191d4c4
koutyuke <gunji.kousuke08@gmail.com>
2023-07-02 09:10:48 +0900

    ⭐(feat)/Homeをα版用に変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,20 @@
 import type { NextPage } from "next";
-import { useSession, signIn, signOut } from "next-auth/react";
+import BaseToast from "components/elements/toast";
 
-const Home: NextPage = () => {
-  const { data } = useSession();
-  if (data) {
-    return (
-      <>
-        Signed in
-        <br />
-        <button className="bg-red-200" type="button" onClick={() => signOut()}>
-          Sign out
-        </button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not Sign in
+const Home: NextPage = () => (
+  <div className="flex h-[calc(100vh_-_10rem)] flex-col items-center justify-center space-x-10 space-y-4 px-10">
+    <BaseToast />
+    <p className="w-full text-center text-4xl">Varsion &alpha;</p>
+    <div className="text-center text-xl">
+      Therefore, there are bugs and unimplemented.
       <br />
-      <button className="bg-gray-200" type="button" onClick={() => signIn("discord")}>
-        Sign in
-      </button>
-    </>
-  );
-};
+      If you have any suggestions, please send them to me via{" "}
+      <a href="https://twitter.com/kusuke0808" rel="noreferrer noopener" target="_blank" className="text-blue-400">
+        Twitter
+      </a>{" "}
+      DM.
+    </div>
+  </div>
+);
 
 export default Home;
