7a76c7cb432b0c9dcfb7b71ddb0680e9460c48a4
koutyuke <gunji.kousuke08@gmail.com>
2023-07-02 20:31:08 +0900

    ❌(delete)/getWithAuthorizationの削除

--- src/libs/axios/getWithAuthorization.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getWithAuthorization = <T>(url: string, token: string) =>
-  axios.get<T>(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-
-export default getWithAuthorization;
