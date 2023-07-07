0d021e7804a36dbc3cf174e99f2d9f5ee397b3f0
koutyuke <gunji.kousuke08@gmail.com>
2023-07-08 01:03:33 +0900

    🔨(fix)/toastの場所の変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,21 @@ import type { NextPage } from "next";
 import BaseToast from "components/elements/toast";
 
 const Home: NextPage = () => (
-  <div className="flex h-full flex-col items-center justify-center space-x-10 space-y-4">
+  <>
     <BaseToast />
-    <p className="w-full text-center text-4xl">Varsion &alpha;</p>
-    <div className="text-center text-xl">
-      Therefore, there are bugs and unimplemented.
-      <br />
-      If you have any suggestions, please send them to me via{" "}
-      <a href="https://twitter.com/kusuke0808" rel="noreferrer noopener" target="_blank" className="text-blue-400">
-        Twitter
-      </a>{" "}
-      DM.
+    <div className="flex h-full flex-col items-center justify-center space-x-10 space-y-4">
+      <p className="w-full text-center text-4xl">Varsion &alpha;</p>
+      <div className="text-center text-xl">
+        Therefore, there are bugs and unimplemented.
+        <br />
+        If you have any suggestions, please send them to me via{" "}
+        <a href="https://twitter.com/kusuke0808" rel="noreferrer noopener" target="_blank" className="text-blue-400">
+          Twitter
+        </a>{" "}
+        DM.
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
