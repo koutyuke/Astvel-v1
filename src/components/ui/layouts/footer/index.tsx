68f271cbd1cd0ea2072417ce12f312f933ff98b0
koutyuke <gunji.kousuke08@gmail.com>
2023-07-08 00:47:15 +0900

    🔨(fix)/各パーツのモジュール化

--- src/components/ui/layouts/footer/index.tsx
+++ src/components/ui/layouts/footer/index.tsx
@@ -1,54 +1,18 @@
 import Image from "next/image";
-import { SiNextdotjs, SiReact, SiTailwindcss, SiExpress, SiSocketdotio } from "react-icons/si";
-import { TbBrandRadixUi } from "react-icons/tb";
-import InternalLink from "components/elements/button/InternalLink";
-import ContentsTitle from "./ContentsTitle";
+import FooterLeftContets from "./left";
+import FooterRightContents from "./right";
+import FooterCenterContents from "./center";
 
 const Footer = () => (
-  <footer className="grid h-60 grid-cols-[50%_50%] grid-rows-[1fr_1.5rem] items-center justify-center bg-gray-800 p-2 text-white">
-    <div className="flex items-center justify-center">
-      <Image src="/icon.PNG" alt="icon" className="" width={80} height={80} />
-      <Image src="/logo.PNG" alt="logo" width={320} height={80} />
+  <footer className="flex grid-cols-[1fr_1fr_1fr] grid-rows-[3.5rem_minmax(6rem,1fr)_2rem] flex-col  gap-x-2 gap-y-4 space-y-3 bg-gray-800 px-2 pt-6 text-white sm:grid sm:space-y-2">
+    <div className="col-[1_/_4] row-start-1 flex h-full items-center justify-center">
+      <Image src="/icon.PNG" alt="icon" className="" width={50} height={50} />
+      <Image src="/logo.PNG" alt="logo" width={200} height={50} />
     </div>
-    <div className="grid h-36 w-full grid-cols-[50%_50%] grid-rows-[50%_50%]">
-      <nav className="row-[1_/_3] flex h-full flex-col items-center">
-        <ContentsTitle imageUrl="/space/artificial-satellite.png" title="Contents" />
-        <ul>
-          <li>
-            <InternalLink url="/guilds" className="">
-              Guilds
-            </InternalLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="flex flex-col items-center justify-start">
-        <ContentsTitle imageUrl="/space/astronaut.png" title="Creater" alt="astronaut" />
-        <ul>
-          <li>
-            <a
-              href="https://twitter.com/kusuke0808"
-              rel="noreferrer noopener"
-              target="_blank"
-              className="text-blue-400"
-            >
-              @kusuke0808
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex h-full w-full flex-col items-center justify-start">
-        <ContentsTitle imageUrl="/space/space-station.png" title="Library" alt="space-station" />
-        <div className="flex space-x-2">
-          <SiNextdotjs size={24} color="white" />
-          <SiReact size={24} />
-          <SiTailwindcss size={24} />
-          <SiExpress size={24} />
-          <SiSocketdotio size={24} />
-          <TbBrandRadixUi size={24} />
-        </div>
-      </div>
-    </div>
-    <span className="col-[1_/_3] row-start-2 w-full text-center">&copy; Astnvel</span>
+    <FooterLeftContets/>
+    <FooterCenterContents/>
+    <FooterRightContents/>
+    <span className="col-[1_/_4] row-start-3 w-full text-center">&copy; Astnvel</span>
   </footer>
 );
 
