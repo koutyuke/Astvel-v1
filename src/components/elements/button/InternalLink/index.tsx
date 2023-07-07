f714fc8559f2479e93288257d355c89b9b4d7eef
koutyuke <gunji.kousuke08@gmail.com>
2023-07-07 19:36:36 +0900

    🔨(feat)/propsの追加

--- src/components/elements/button/InternalLink/index.tsx
+++ src/components/elements/button/InternalLink/index.tsx
@@ -4,9 +4,9 @@ import { ComponentPropsWithoutRef, FC } from "react";
 type Props = {
   url: string;
   children: string;
-} & Pick<ComponentPropsWithoutRef<"div">, "className">;
+} & ComponentPropsWithoutRef<"button">;
 
-const InternalLink: FC<Props> = ({ url, children: text, className }) => {
+const InternalLink: FC<Props> = ({ url, children: text, className, onClick, ...other }) => {
   const router = useRouter();
   if (router.pathname === url) {
     return <div className={`underline decoration-white underline-offset-2 ${className}`}>{text}</div>;
@@ -15,8 +15,14 @@ const InternalLink: FC<Props> = ({ url, children: text, className }) => {
   return (
     <button
       type="button"
-      onClick={() => router.push(url)}
+      onClick={e => {
+        router.push(url);
+        if (onClick) {
+          onClick(e);
+        }
+      }}
       className={`${className} rounded-md outline-offset-4 outline-orange-500 hover:outline`}
+      {...other}
     >
       {text}
     </button>
