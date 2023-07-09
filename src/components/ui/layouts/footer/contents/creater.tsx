afb604d94aebb0e2c4a1d53d9292c9299a0f2be0
koutyuke <gunji.kousuke08@gmail.com>
2023-07-09 13:24:04 +0900

    ⭐(create)/footerのcreaterの作成

---
+++ src/components/ui/layouts/footer/contents/creater.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef, FC } from "react";
+import { FaUserAstronaut } from "react-icons/fa";
+
+import Title from "./title";
+
+type Props = ComponentPropsWithoutRef<"div">;
+
+const Creater: FC<Props> = ({ className, ...other }) => (
+  <div className={`flex  flex-col items-center justify-start space-y-1 ${className}`} {...other}>
+    <Title title="Creater" Icon={FaUserAstronaut} />
+    <a href="https://twitter.com/kusuke0808" rel="noreferrer noopener" target="_blank" className="text-blue-400">
+      @kusuke0808
+    </a>
+  </div>
+);
+
+export default Creater;
