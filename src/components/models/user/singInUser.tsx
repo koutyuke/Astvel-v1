9f7b09d6dd9356216998ef7b2eecee8486d30bf5
koutyuke <gunji.kousuke08@gmail.com>
2023-07-10 17:10:00 +0900

    🔨(fix)/z-indexの追加

--- src/components/models/user/singInUser.tsx
+++ src/components/models/user/singInUser.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const SignInUser: FC<Props> = ({ image, main, sub }) => (
   <Avatar.Root className="relative h-14 min-w-[10rem] overflow-auto ">
-    <div className="absolute left-0 top-0 h-14 w-14 rounded-full bg-white p-[3px]">
+    <div className="absolute left-0 top-0 z-[1] h-14 w-14 rounded-full bg-white p-[3px]">
       <Avatar.Image src={image} className="h-[50px] w-[50px] rounded-full" />
     </div>
     <div className="my-1 ml-8 h-12 min-w-[8rem] rounded-r-xl bg-white py-1 pl-6 pr-1">
