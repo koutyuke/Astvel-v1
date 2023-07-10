3d7dd7a388383690aff5cb79a5fae2d525f84202
koutyuke <gunji.kousuke08@gmail.com>
2023-07-10 16:59:23 +0900

    🔨(feat)/背景としてdivタグを追加

--- src/components/models/user/singInUser.tsx
+++ src/components/models/user/singInUser.tsx
@@ -9,7 +9,9 @@ type Props = {
 
 const SignInUser: FC<Props> = ({ image, main, sub }) => (
   <Avatar.Root className="relative h-14 min-w-[10rem] overflow-auto ">
-    <Avatar.Image src={image} className="absolute left-0 top-0 h-14 w-14 rounded-full border-[3px] border-white" />
+    <div className="absolute left-0 top-0 h-14 w-14 rounded-full bg-white p-[3px]">
+      <Avatar.Image src={image} className="h-[50px] w-[50px] rounded-full" />
+    </div>
     <div className="my-1 ml-8 h-12 min-w-[8rem] rounded-r-xl bg-white py-1 pl-6 pr-1">
       <div className="flex h-10 min-w-[6rem] max-w-full flex-col items-center justify-center overflow-hidden text-ellipsis rounded-lg bg-gray-600 px-2 py-1 text-white">
         <span>{main}</span>
