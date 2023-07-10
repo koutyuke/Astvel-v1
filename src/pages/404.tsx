050840757761b2e48f5fa2fc1bf777e722b3009e
koutyuke <gunji.kousuke08@gmail.com>
2023-07-10 09:37:59 +0900

    🔨(feat)/404の変更

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,20 @@
-import ErrorMessage from "components/ui/errorMessage";
 import { NextPage } from "next";
+import Image from "next/image";
 
 const NotFound: NextPage = () => (
-  <div className="flex h-full items-center justify-center">
-    <ErrorMessage title="404">Page Not Found</ErrorMessage>
+  <div className="flex h-full flex-col items-center justify-center space-y-4">
+    <div className="flex h-20 items-center text-7xl">
+      <span>4</span>
+      <Image src="/icon.PNG" alt="icon" className="" width={75} height={75} />
+      <span>4</span>
+    </div>
+    <p className="text-2xl">Oops! Page Not Found</p>
+    <p className="text-center">
+      We can&apos;t find the page <br className="sm:hidden" />
+      you&apos;re looking for.
+      <br />
+      Please check URL.
+    </p>
   </div>
 );
 
