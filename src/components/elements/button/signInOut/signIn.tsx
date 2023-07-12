7d4282038062fbf44739c7432f5d497e2f72de70
koutyuke <gunji.kousuke08@gmail.com>
2023-07-12 13:43:06 +0900

    ⭐(create)/SignInの作成

---
+++ src/components/elements/button/signInOut/signIn.tsx
@@ -0,0 +1,18 @@
+import { signIn } from "next-auth/react";
+import { ComponentPropsWithoutRef, FC } from "react";
+
+
+type SignOutProps = ComponentPropsWithoutRef<"button">;
+
+const SignInButton: FC<SignOutProps> = ({ className, ...other }) => (
+  <button
+    type="button"
+    onClick={() => signIn("discord")}
+    className={`flex h-10 w-40 items-center justify-center rounded-full bg-[#ff4da6] outline-2 ${className}`}
+    {...other}
+  >
+    <span className="text-lg">Sign In</span>
+  </button>
+)
+
+export default SignInButton
