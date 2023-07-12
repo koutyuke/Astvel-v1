6a9226a2da3e1288ffb58670c903d0fb8b94a0f2
koutyuke <gunji.kousuke08@gmail.com>
2023-07-12 11:21:40 +0900

    🔨(fix)/headerの背景色の変更

--- src/components/ui/layouts/header/index.tsx
+++ src/components/ui/layouts/header/index.tsx
@@ -5,7 +5,7 @@ import HeaderLinks from "./links";
 import HamburgerMenu from "./hamburger";
 
 const Header: FC = () => (
-  <header className="sticky top-0 z-50 flex h-20 w-full grid-cols-[10rem_1fr_10rem] justify-between bg-gray-800 px-6 py-4 sm:grid md:h-24 md:grid-cols-[12.5rem_1fr_12.5rem]">
+  <header className="sticky top-0 z-50 flex h-20 w-full grid-cols-[10rem_1fr_10rem] justify-between px-6 py-4 backdrop-blur sm:grid md:h-24 md:grid-cols-[12.5rem_1fr_12.5rem]">
     <div className="flex h-full w-40 items-center justify-center md:w-[12.5rem]">
       <Image src="/logo.PNG" alt="logo" width={200} height={50} />
     </div>
