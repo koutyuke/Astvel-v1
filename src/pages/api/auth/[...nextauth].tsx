34c9e3fb4d7eaf8ecad142622c244dad91b4a7fd
koutyuke <gunji.kousuke08@gmail.com>
2023-07-12 13:48:28 +0900

    ⭐(create)/SignInページのの作成

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -39,4 +39,7 @@ export default nextAuth({
       return setSession;
     },
   },
+  pages: {
+    signIn: "/auth/signin",
+  },
 });
---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,27 @@
+import SignInButton from "components/elements/button/signInOut/signIn";
+import { NextPage } from "next";
+import Image from "next/image";
+import { FaDiscord } from "react-icons/fa";
+import { TbCirclesRelation } from "react-icons/tb";
+
+const Signin: NextPage = () => (
+  <div className="flex h-full w-full max-w-[30rem] flex-col items-center justify-center space-y-4 rounded-xl text-center">
+    <p className="text-5xl">Signin</p>
+    <p className="">
+      This application operates a Discord bot.
+      <br />
+      Therefore, please log in with your Discord account.
+    </p>
+    <div className="flex h-32 w-full items-center justify-center space-x-4">
+      <div className="flex h-20 w-20 items-center justify-center rounded-2xl bg-teal-700 p-1 sm:h-24 sm:w-24 sm:rounded-3xl">
+        <Image src="/icon.PNG" alt="icon" width={100} height={100} />
+      </div>
+      <TbCirclesRelation className="" size={60} />
+      <div className="flex h-20 w-20 items-center justify-center rounded-2xl bg-blue-700  p-2 sm:h-24 sm:w-24 sm:rounded-3xl">
+        <FaDiscord size={100} />
+      </div>
+    </div>
+    <SignInButton />
+  </div>
+);
+export default Signin;
