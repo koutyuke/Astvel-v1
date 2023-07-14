35361bde9dd061d1f0d75d39b2b395cc50c42d37
koutyuke <gunji.kousuke08@gmail.com>
2023-07-14 18:58:53 +0900

    ⭐(create)/sign outページの作成

---
+++ src/pages/auth/signout.tsx
@@ -0,0 +1,20 @@
+import SignOutButton from "components/elements/button/signInOut/signOut";
+import { NextPage } from "next";
+import Image from "next/image";
+
+const Signout:NextPage = () => (
+  <div className="flex h-full w-full max-w-[30rem] flex-col items-center justify-center space-y-4 text-center">
+    <p className="text-5xl">Sign Out</p>
+    <p className="">
+      Oh, you&apos;re signing out?
+      <br />
+      That&apos;s a little disappointing.....
+    </p>
+    <div className="flex h-32 w-full items-center justify-center">
+      <Image src="/space/astronaut-ingravity.png" alt="astronaut" width={150} height={150}/>
+    </div>
+    <SignOutButton className="h-10 w-40 rounded-full bg-[#ff4da6]"/>
+  </div>
+)
+
+export default Signout
--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -17,6 +17,18 @@ const Information: NextPage = () => (
         </a>
       </div>
     </div>
+    <div>
+      <div>
+        {" "}
+        Icons made by Prashanth Rapolu 15 from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+    <div>
+    <div> Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
   </div>
 );
 
