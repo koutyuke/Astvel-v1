aeb5a4f7e5d0931e6ea5d33dbff73e97e27ef8f4
koutyuke <gunji.kousuke08@gmail.com>
2023-07-19 14:45:50 +0900

    style: 💄 (UserModel) overflow時に...になるように修正

--- src/components/elements/button/signInOut/small.tsx
+++ src/components/elements/button/signInOut/small.tsx
@@ -7,7 +7,7 @@ import SignInButton from "./signIn";
 
 const SignInOutSmall: FC = () => {
   const { session, status } = useValidatedSession();
-  const [isOpenLogout, setOpenLogout] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   if (status === "loading") {
     return (
       <div className="flex h-10 w-40 items-center justify-center rounded-full bg-[#ff4da6]">
@@ -20,14 +20,14 @@ const SignInOutSmall: FC = () => {
     const { user } = session.data;
     return (
       <div
-        className={`${isOpenLogout ? "" : " hover:outline"} relative rounded-md outline-offset-4 outline-orange-500 `}
+        className={`${isOpen ? "" : " hover:outline"} relative max-w-full rounded-md outline-offset-4 outline-orange-500 `}
         onMouseLeave={() => {
-          if (isOpenLogout) {
-            setOpenLogout(false);
+          if (isOpen) {
+            setOpen(false);
           }
         }}
       >
-        {isOpenLogout && (
+        {isOpen && (
           <button
             type="button"
             className="absolute left-[2.6rem] top-10 flex h-[3.15rem] w-[7.4rem] items-center justify-center rounded-b-xl bg-[#ff4da6] pt-2 hover:bg-green-500"
@@ -36,7 +36,7 @@ const SignInOutSmall: FC = () => {
             <span>Sign Out</span>
           </button>
         )}
-        <button type="button" onClick={() => setOpenLogout(!isOpenLogout)} className="z-10">
+        <button type="button" onClick={() => setOpen(!isOpen)} className="z-10 w-full">
           <SignInUser
             image={iconUrlGen(user.provider_id ?? "", user.avatar ?? "")}
             main={user.discriminator === "0" ? user.global_name ?? "" : user.username ?? ""}
--- src/components/models/user/singInUser.tsx
+++ src/components/models/user/singInUser.tsx
@@ -8,14 +8,14 @@ type Props = {
 };
 
 const SignInUser: FC<Props> = ({ image, main, sub }) => (
-  <Avatar.Root className="relative h-14 min-w-[10rem] overflow-auto ">
+  <Avatar.Root className="relative h-14 min-w-[10rem] max-w-full">
     <div className="absolute left-0 top-0 z-[1] h-14 w-14 rounded-full bg-white p-[3px]">
       <Avatar.Image src={image} className="h-[50px] w-[50px] rounded-full" />
     </div>
-    <div className="my-1 ml-8 h-12 min-w-[8rem] rounded-r-xl bg-white py-1 pl-6 pr-1">
-      <div className="flex h-10 min-w-[6rem] max-w-full flex-col items-center justify-center overflow-hidden text-ellipsis rounded-lg bg-gray-600 px-2 py-1 text-white">
-        <span>{main}</span>
-        <span className="text-[6px]">{sub}</span>
+    <div className="my-1 ml-8 h-12 min-w-[8rem] max-w-full rounded-r-xl bg-white py-1 pl-6 pr-1">
+      <div className="flex h-10 min-w-[6rem] flex-col items-center justify-center rounded-lg bg-gray-600 px-2 py-1 text-white">
+        <span className="max-w-full overflow-hidden text-ellipsis">{main}</span>
+        <span className="max-w-full overflow-hidden text-ellipsis text-[6px]">{sub}</span>
       </div>
     </div>
   </Avatar.Root>
