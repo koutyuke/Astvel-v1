a55f807b94392676a1bfa2ad05b7552f52bb8459
koutyuke <gunji.kousuke08@gmail.com>
2023-08-06 19:09:38 +0900

    feat: 🎈 (tooltitle) 「tool」からGuildの名前に変更

--- src/featutres/dnd/components/ui/toolbar/index.tsx
+++ src/featutres/dnd/components/ui/toolbar/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 const ToolBar: FC<Props> = ({ guildId }) => (
   <div className="relative">
     <div className="group absolute -left-16 top-0 flex h-full w-16 flex-col space-y-2 overflow-y-auto overflow-x-hidden rounded-md bg-gradient-to-b from-[#22c1c3] to-[#7cff9d] p-2 text-teal-700 shadow-lg duration-200 hover:w-40">
-      <ToolTitle />
+      <ToolTitle guildId={guildId} />
       <span className="h-[2px] w-full rounded-full bg-[rgba(255,255,255,0.6)]" />
       <ToolMoveMenu guildId={guildId} />
       <ToolResetMenu />
