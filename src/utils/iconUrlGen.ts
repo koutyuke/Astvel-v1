9186d37d5377308c0da2a77bea1fe51cefb1f3b3
koutyuke <gunji.kousuke08@gmail.com>
2023-08-06 19:03:09 +0900

    feat: 🎈 (iconUrlGen) guildIconの生成関数の追加

--- src/utils/iconUrlGen.ts
+++ src/utils/iconUrlGen.ts
@@ -1,4 +1,8 @@
-const iconUrlGen = (id: string, avatar: string | null) =>
-  avatar ? `https://cdn.discordapp.com/avatars/${id}/${avatar}.png` : "https://cdn.discordapp.com/embed/avatars/0.png";
+const baseUrl = "https://cdn.discordapp.com/embed/avatars/0.png";
 
-export default iconUrlGen;
+const avatarUrlGen = (id: string, avatar: string | null) =>
+  avatar ? `https://cdn.discordapp.com/avatars/${id}/${avatar}.png` : baseUrl;
+
+const guildIconUrlGen = (id: string, icon: string) => `https://cdn.discordapp.com/icons/${id}/${icon}.png`;
+
+export { avatarUrlGen, guildIconUrlGen };
