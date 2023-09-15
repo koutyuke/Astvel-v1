e7a79067b1f9c0f1050ee4af64c3950f3f66714b
koutyuke <gunji.kousuke08@gmail.com>
2023-09-15 19:44:56 +0900

    feat: 🎈 (footer) 新デザインのフッターの追加

--- src/components/layouts/footer/contents/index.ts
+++
@@ -1,5 +0,0 @@
-export { Creater } from "./creater";
-export { Framework } from "./framework";
-export { Library } from "./library";
-export { Navigate } from "./navigate";
-export { Repository } from "./repository";
--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -1,20 +1,66 @@
 import Image from "next/image";
-import { Creater, Framework, Library, Navigate, Repository } from "./contents";
+import { ExternalLink, InternalLink } from "components/elements/link";
+import { GithubIcon } from "components/icon/github";
+import { DiscordIcon } from "components/icon/discord";
 
 const Footer = () => (
-  <footer className="flex grid-rows-[3.5rem_minmax(6rem,1fr)_1.5rem] flex-col  space-y-6 bg-gray-800 px-2 pb-2 pt-6 text-white sm:grid sm:gap-x-2 sm:gap-y-4 sm:space-y-0">
-    <div className="row-start-1 flex h-full items-center justify-center">
-      <Image src="/icon.PNG" alt="icon" className="" width={50} height={50} />
-      <Image src="/logo.PNG" alt="logo" width={200} height={50} />
+  <footer className="flex w-full flex-col justify-center space-y-8 border-t border-gray-500 px-8 py-16 tablet:p-16">
+    <div className="grid h-full grid-cols-12 grid-rows-3 justify-items-center gap-6 border-b border-gray-500 pb-8 tablet:grid-rows-2 laptop:grid-rows-1">
+      <div className="col-start-2 col-end-12 row-start-1 flex h-full flex-col items-center justify-start laptop:col-start-2 laptop:col-end-7">
+        <div className="flex h-16 items-center">
+          <Image src="/icon.PNG" alt="icon" className="" width={64} height={64} />
+          <Image src="/logo.PNG" alt="logo" width={160} height={40} />
+        </div>
+        <div className="flex space-x-2">
+          <ExternalLink url="https://github.com/koutyuke/Astvel">
+            <GithubIcon className="fill-white transition hover:fill-green-500" />
+          </ExternalLink>
+          <ExternalLink url="https://discordapp.com/users/1078178551624368229">
+            <DiscordIcon className="fill-white transition hover:fill-green-500" />
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="col-start-2 col-end-7 row-start-2 flex h-full flex-col space-y-3 tablet:col-end-5 laptop:col-start-7 laptop:col-end-9 laptop:row-start-1">
+        <p className="text-xl font-bold">Content</p>
+        <InternalLink path="/" className="text-xs">
+          <p>Home</p>
+        </InternalLink>
+        <InternalLink path="/guilds" className="text-xs">
+          <p className="">Select-Guild</p>
+        </InternalLink>
+        <InternalLink path="/usage" className="text-xs">
+          <p>Usage</p>
+        </InternalLink>
+        <InternalLink path="/information" className="text-xs">
+          <p>Information</p>
+        </InternalLink>
+      </div>
+      <div className="col-start-7 col-end-12  row-start-2 h-full space-y-3 tablet:col-start-5 tablet:col-end-9 laptop:col-start-9 laptop:col-end-11 laptop:row-start-1">
+        <p className="text-xl font-bold">About</p>
+        <ExternalLink url="https://github.com/koutyuke/Astvel" className="text-xs" icon>
+          <p>Code(Github)</p>
+        </ExternalLink>
+        <ExternalLink url="" className="text-xs" icon>
+          <p>Licens(Github)</p>
+        </ExternalLink>
+        <ExternalLink url="" className="text-xs" icon>
+          <p>Design(Figma)</p>
+        </ExternalLink>
+      </div>
+      <div className="col-start-2 col-end-7 row-start-3 h-full space-y-3  tablet:col-start-9  tablet:col-end-12 tablet:row-start-2 laptop:col-start-11 laptop:col-end-13 laptop:row-start-1">
+        <p className="text-xl font-bold">Creator</p>
+        <ExternalLink url="https://twitter.com/kusuke0808" icon className="text-xs">
+          <p>X(Twitter)</p>
+        </ExternalLink>
+        <ExternalLink url="https://github.com/koutyuke" icon className="text-xs">
+          <p>Github</p>
+        </ExternalLink>
+        <ExternalLink url="https://discordapp.com/users/687588356875354126" icon className="text-xs">
+          <p>Discord</p>
+        </ExternalLink>
+      </div>
     </div>
-    <div className="flex grid-cols-3 grid-rows-2 flex-col space-y-6 sm:grid sm:space-y-0">
-      <Navigate className="order-1 col-start-1 row-[1_/_3]" />
-      <Creater className="order-2 col-start-2 row-start-1" />
-      <Framework className="order-4 col-start-3 row-start-1" />
-      <Repository className="order-3 col-start-2 row-start-2" />
-      <Library className="order-5 col-start-3 row-start-2" />
-    </div>
-    <span className="row-start-3 h-6 w-full text-center">&copy; Astnvel</span>
+    <p className="h-4 text-center">&copy; Astvel</p>
   </footer>
 );
 
