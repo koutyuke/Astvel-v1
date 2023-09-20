6fd2b2622f231b8f7252abc78eeefe3f83d0ec72
koutyuke <gunji.kousuke08@gmail.com>
2023-09-20 11:39:15 +0900

    feat: ✨ (icon/rocket) `rocketIcon`コンポーネントの作成

---
+++ src/components/icon/rocket/rocket.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+
+const RocketIcon: IconType = props => <BsFillRocketTakeoffFill {...props} />;
+
+export { RocketIcon };
