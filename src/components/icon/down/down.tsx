0f3e40b56029a6b983e59a888ac9e121ac1aaa3e
koutyuke <gunji.kousuke08@gmail.com>
2023-09-20 11:38:16 +0900

    feat: ✨ (icon/down) `downIcon`コンポーネントの作成

---
+++ src/components/icon/down/down.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { IoChevronDown } from "react-icons/io5";
+
+const DownIcon: IconType = props => <IoChevronDown {...props} />;
+
+export { DownIcon };
