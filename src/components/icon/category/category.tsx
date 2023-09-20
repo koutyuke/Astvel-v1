81c5f13167f21aa4b806f6e0269432aa36e92c6c
koutyuke <gunji.kousuke08@gmail.com>
2023-09-20 11:38:02 +0900

    feat: ✨ (icon/category) `categoryIcon`コンポーネントの作成

---
+++ src/components/icon/category/category.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { BiCategory } from "react-icons/bi";
+
+const CategoryIcon: IconType = props => <BiCategory {...props} />;
+
+export { CategoryIcon };
