8225d8a85b1274d0b851e245523074b845f21449
koutyuke <gunji.kousuke08@gmail.com>
2023-09-20 11:38:33 +0900

    feat: ✨ (icon/grid) `gridIcon`コンポーネントの作成

---
+++ src/components/icon/grid/grid.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { IoGridOutline } from "react-icons/io5";
+
+const GridIcon: IconType = props => <IoGridOutline {...props} />;
+
+export { GridIcon };
