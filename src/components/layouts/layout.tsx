599b5d55d2c6594bb0e1d901c917f2a66da9ed24
koutyuke <gunji.kousuke08@gmail.com>
2023-09-20 12:28:01 +0900

    style: 💄 (layout) `main`タグにclassName属性を付与

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,17 +1,24 @@
-import { ComponentProps, FC, ReactNode } from "react";
+import { ComponentProps, ComponentPropsWithoutRef, FC, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 import { Seo } from "./seo";
 import { Header } from "./header";
 import { Footer } from "./footer";
 
 type Props = {
   children: ReactNode;
-} & ComponentProps<typeof Seo>;
+} & ComponentProps<typeof Seo> &
+  Pick<ComponentPropsWithoutRef<"main">, "className">;
 
-const Layout: FC<Props> = ({ children, title, description }) => (
+const Layout: FC<Props> = ({ children, title, description, className }) => (
   <>
     <Seo title={title} description={description} />
     <Header />
-    <main className="m-auto flex h-0 min-h-[calc(100vh_-_4rem)] w-full max-w-[100rem] items-center justify-center px-10 py-8">
+    <main
+      className={twMerge(
+        "m-auto flex h-0 min-h-[calc(100vh_-_4rem)] w-full max-w-[100rem] items-center justify-center py-4",
+        className,
+      )}
+    >
       {children}
     </main>
     <Footer />
