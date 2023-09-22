b2594787ed4246549d5b9fed9ec2d0e726e62ba5
koutyuke <gunji.kousuke08@gmail.com>
2023-09-22 11:00:30 +0900

    feat: ✨ (icon) `DragIcon`コンポーネントの作成

---
+++ src/components/icon/drag/drag.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { RxDragHandleDots2 } from "react-icons/rx";
+
+const DragIcon: IconType = props => <RxDragHandleDots2 {...props} />;
+
+export { DragIcon };
