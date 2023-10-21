7af25daa968d8d71ddcd7a3cce15c158023d533c
koutyuke <gunji.kousuke08@gmail.com>
2023-10-21 22:50:22 +0900

    feat: ✨ (utils/isPrivateChannel) `privateChannel`かどうかの判定関数の作成

---
+++ src/utils/isPrivateVoiceChannel.ts
@@ -0,0 +1,12 @@
+import { Permission } from "types/api/astvel";
+import { permissionCheck } from "./permissionCheck";
+
+const isPrivateVoiceChannel = (guildId: string, permissionOverwriteRoles: Permission[]): boolean => {
+  const everyone = permissionOverwriteRoles.find(role => role.id === guildId);
+  const isPrivate =
+    everyone !== undefined
+      ? permissionCheck(BigInt(everyone.deny), BigInt(1024)) && permissionCheck(BigInt(everyone.deny), BigInt(1048576))
+      : false;
+  return isPrivate;
+};
+export { isPrivateVoiceChannel };
