707470e9eb7541e747ab42bcc3d67fe17dd3ac11
koutyuke <gunji.kousuke08@gmail.com>
2023-10-21 22:33:45 +0900

    feat: 🎈 (Layout) footer非表示用のオプションを追加

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -6,23 +6,28 @@ import { Footer } from "./footer";
 
 type Props = {
   children: ReactNode;
+  footerHidden?: boolean;
 } & ComponentProps<typeof Seo> &
   Pick<ComponentPropsWithoutRef<"main">, "className">;
 
-const Layout: FC<Props> = ({ children, title, description, className }) => (
+const Layout: FC<Props> = ({ children, title, description, footerHidden, className }) => (
   <>
     <Seo title={title} description={description} />
     <Header />
     <main
       className={twMerge(
-        "m-auto flex h-0 min-h-[calc(100vh_-_4rem)] w-full max-w-[100rem] items-center justify-center py-4",
+        "m-auto flex h-0 min-h-[calc(100vh_-_4rem)] w-full max-w-[80rem] items-center justify-center py-4",
         className,
       )}
     >
       {children}
     </main>
-    <Footer />
+    {!footerHidden && <Footer />}
   </>
 );
+
+Layout.defaultProps = {
+  footerHidden: false,
+};
 
 export { Layout };
