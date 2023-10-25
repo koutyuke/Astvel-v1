673f595954eedc15fa9f4ef0573870f2a1263cf0
koutyuke <gunji.kousuke08@gmail.com>
2023-10-26 00:56:56 +0900

    feat: 🎈 (emojiPicker) Propsの変更

--- src/featutres/emojiPicker/components/picker.tsx
+++ src/featutres/emojiPicker/components/picker.tsx
@@ -1,22 +1,17 @@
-import { EmojiClickData, EmojiStyle, Theme } from "emoji-picker-react";
+import { EmojiStyle, Theme } from "emoji-picker-react";
 import dynamic from "next/dynamic";
 import { ComponentPropsWithoutRef, FC } from "react";
 
 const Picker = dynamic(() => import("emoji-picker-react"), { ssr: false });
 
-type Props = {
-  setClose: (value: boolean) => void;
-  setValue: (value: EmojiClickData) => void;
-} & Pick<ComponentPropsWithoutRef<"div">, "className">;
+type Props = Pick<ComponentPropsWithoutRef<typeof Picker>, "onEmojiClick"> &
+  Pick<ComponentPropsWithoutRef<"div">, "className">;
 
-const EmojiPicker: FC<Props> = ({ setClose, setValue, className }) => {
+const EmojiPicker: FC<Props> = ({ onEmojiClick, className }) => {
   return (
     <div className={className}>
       <Picker
-        onEmojiClick={emojiData => {
-          setClose(false);
-          setValue(emojiData);
-        }}
+        onEmojiClick={onEmojiClick}
         width="100%"
         height="100%"
         emojiStyle={EmojiStyle.NATIVE}
