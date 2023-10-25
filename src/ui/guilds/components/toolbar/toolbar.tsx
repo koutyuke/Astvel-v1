d7f9a647728e6323f3f1d07422f50318795ec130
koutyuke <gunji.kousuke08@gmail.com>
2023-10-26 01:20:07 +0900

    style: 💄 (tools/) レスポンシブ対応

--- src/ui/guilds/components/toolbar/toolbar.tsx
+++ src/ui/guilds/components/toolbar/toolbar.tsx
@@ -1,23 +1,29 @@
 import { FC } from "react";
-import { SelectedGuild } from "./selectedGuild";
+import { twMerge } from "tailwind-merge";
+import { SelectedGuild } from "./guild";
+import { Separator } from "./separator";
+import { TravelerSize } from "./travelerSize";
+import { BackToPage } from "./backToPage";
 import { Move } from "./move";
 import { Reset } from "./reset";
-import { Setting } from "./setting";
-import { BackGuilds } from "./backGuilds";
+import { TeamSetting } from "./teamSetting";
 
 type Props = {
   guildId: string;
 };
 const ToolBar: FC<Props> = ({ guildId }) => (
-  <div className="relative">
-    <div className="group absolute -left-16 top-0 flex h-full w-16 flex-col overflow-y-auto overflow-x-hidden rounded-md bg-gradient-to-b from-[#22c1c3] to-[#7cff9d] p-2 text-teal-700 shadow-lg duration-200 hover:w-40 [&>*]:mb-2 last:[&>*]:mb-0 last:[&>*]:mt-auto">
-      <SelectedGuild guildId={guildId} />
-      <span className="h-[2px] w-full rounded-full bg-[rgba(255,255,255,0.6)]" />
+  <div className="mx-8 flex h-14 items-center justify-start rounded-lg border border-gray-500 p-2 tablet:mx-16">
+    <SelectedGuild guildId={guildId} />
+    <Separator />
+    <TravelerSize />
+    <Separator />
+    <div className={twMerge("scrollber-hidden flex w-0 flex-1 items-center space-x-2 overflow-auto px-2")}>
       <Move guildId={guildId} />
       <Reset />
-      <Setting />
-      <BackGuilds />
+      <TeamSetting />
     </div>
+    <Separator />
+    <BackToPage />
   </div>
 );
 
