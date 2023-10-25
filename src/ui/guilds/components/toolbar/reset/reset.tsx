371de89699d4026c8294f2e692c8693ad05f445e
koutyuke <gunji.kousuke08@gmail.com>
2023-10-26 01:14:10 +0900

    feat: 🎈 (tools/reset) `メンバーリセット`機能のモーダルデザインの変更

--- src/ui/guilds/components/toolbar/reset/reset.tsx
+++ src/ui/guilds/components/toolbar/reset/reset.tsx
@@ -1,36 +1,51 @@
 import { FC, useState } from "react";
-import { TbReload } from "react-icons/tb";
 import { createDialog } from "components/elements/dialog";
-// import { useSetToast } from "featutres/toast/hooks";
+import { useSetToast } from "featutres/toast/hooks";
 import { BaseButton } from "components/elements/button";
-import { ToolTriggerButton } from "../triggerButton";
+import { ResetIcon } from "components/icon/reset";
+import { useMemberReset } from "ui/guilds/hooks/useMemberReset";
 
 const Reset: FC = () => {
   const [open, setOpen] = useState(false);
-  const AddDialog = createDialog(<ToolTriggerButton Icon={TbReload} title="Reset" />);
-  // const toastSetter = useSetToast();
+  const AddDialog = createDialog(
+    <span>
+      <ResetIcon className=" stroke-gray-400 transition hover:stroke-green-500" size={26} />
+    </span>,
+  );
+  const toastSetter = useSetToast();
+
+  const reset = useMemberReset();
 
   return (
     <AddDialog open={open} setOpen={setOpen}>
-      <div className="flex h-48 w-80 flex-col items-center justify-between rounded-lg bg-white p-4 text-gray-500">
+      <div className="flex aspect-video w-96 max-w-[90vw] flex-col items-center justify-between space-y-6 rounded-lg bg-black-1 p-6 text-white outline outline-1 outline-gray-500">
         <p className="w-full text-center text-2xl">Member Reset</p>
         <p className="w-full text-center">
           Reset all members.
           <br />
           Are you sure?
-          <br />* this action don&apos;t reset teams.
+          <br />* This action don&apos;t reset teams.
         </p>
 
         <div className="flex w-full items-center justify-between">
           <BaseButton
-            className="h-8 border-2 border-gray-300 duration-200 hover:scale-110"
+            theme="nomal"
+            className="h-8"
             onClick={() => {
               setOpen(false);
             }}
           >
-            <p>Calcel</p>
+            <p>Cancel</p>
           </BaseButton>
-          <BaseButton className="h-8 bg-green-300 text-green-700 duration-200 hover:scale-110" onClick={() => {}}>
+          <BaseButton
+            theme="danger"
+            className="h-8"
+            onClick={() => {
+              setOpen(false);
+              reset();
+              toastSetter({ title: "Reset", message: "Success for Member Reset", status: "success" });
+            }}
+          >
             <p>Reset</p>
           </BaseButton>
         </div>
