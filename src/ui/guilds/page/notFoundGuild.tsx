a9a468ec736a3bf48f8146291fa0da1aa5bfaf14
koutyuke <gunji.kousuke08@gmail.com>
2023-10-26 15:38:34 +0900

    feat: ✨ (ui/guild/page) `notFoundGuild`ページコンポーネントの作成

---
+++ src/ui/guilds/page/notFoundGuild.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import Image from "next/image";
+
+const NotFoundGuild: FC = () => (
+  <div className="grid grid-rows-[3rem_,_8rem_,_1fr] items-center justify-items-center gap-4 tablet:grid-cols-[8rem_,_18rem] tablet:grid-rows-[3rem_,_1fr]">
+    <p className="text-4xl">No Guild</p>
+    <div className="h-fit w-fit tablet:col-span-1 tablet:row-[1_/_3]">
+      <Image
+        src="/space/crashed-rocket.png"
+        alt="crashed-rocket image"
+        priority
+        loading="eager"
+        height={128}
+        width={128}
+      />
+    </div>
+    <div className="flex flex-col items-center justify-center space-y-2 text-center">
+      <p>Guild doesn&apos;t exist or has no bots in it.</p>
+      <p className="leading-6">
+        Please check the URL or
+        <br />
+        invite the bot to the guild.
+      </p>
+    </div>
+  </div>
+);
+
+export { NotFoundGuild };
