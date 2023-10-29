0d941c49c2e6d33cc197dacfae6322c79b50e5bc
koutyuke <gunji.kousuke08@gmail.com>
2023-10-29 20:48:33 +0900

    feat: 🎈 (schema/sesison) 型変更に伴うスキーマの変更

--- src/stores/schema/session.ts
+++ src/stores/schema/session.ts
@@ -1,17 +1,14 @@
 import { z } from "zod";
 
+const userSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string().nullable(),
+  image: z.string().nullable(),
+});
+
 const sessionSchema = z.object({
-  user: z.object({
-    id: z.string(),
-    provider_id: z.string(),
-    username: z.string(),
-    global_name: z.string(),
-    avatar: z.string().nullable(),
-    discriminator: z.string(),
-    banner_color: z.string().nullable(),
-    access_token: z.string(),
-    refresh_token: z.string(),
-  }),
+  user: userSchema,
   accessToken: z.string(),
   refreshToken: z.string(),
 });
