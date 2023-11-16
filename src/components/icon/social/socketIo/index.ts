37ed0d2a39ea30066962349120b3ab0ad166bf7e
koutyuke <gunji.kousuke08@gmail.com>
2023-11-17 08:35:42 +0900

    feat: ✨ (icons/social/socketio) `socket.io`のアイコンの作成

---
+++ src/components/icon/social/socketIo/index.ts
@@ -0,0 +1,1 @@
+export { SocketIoIcon } from "./socketIo";
---
+++ src/components/icon/social/socketIo/socketIo.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { SiSocketdotio } from "react-icons/si";
+
+const SocketIoIcon: IconType = SiSocketdotio;
+
+export { SocketIoIcon };
