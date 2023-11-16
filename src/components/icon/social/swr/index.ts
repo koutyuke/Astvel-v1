71e9257d582532a5781de1e8b948d02f59b9dda8
koutyuke <gunji.kousuke08@gmail.com>
2023-11-17 08:36:11 +0900

    feat: ✨ (icons/social/swr) `swr`のアイコンの作成

---
+++ src/components/icon/social/swr/index.ts
@@ -0,0 +1,1 @@
+export { SwrIcon } from "./swr";
---
+++ src/components/icon/social/swr/swr.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { SiSwr } from "react-icons/si";
+
+const SwrIcon: IconType = SiSwr;
+
+export { SwrIcon };
