6b38278f6ac20610e48591aed9b0d05d2abbc353
koutyuke <gunji.kousuke08@gmail.com>
2023-11-17 08:35:12 +0900

    feat: ✨ (icons/social/radixUI) `radix ui`のアイコンの作成

---
+++ src/components/icon/social/radixUI/index.ts
@@ -0,0 +1,1 @@
+export { RadixUIIcon } from "./radixUI";
---
+++ src/components/icon/social/radixUI/radixUI.tsx
@@ -0,0 +1,6 @@
+import { IconType } from "react-icons";
+import { SiRadixui } from "react-icons/si";
+
+const RadixUIIcon: IconType = SiRadixui;
+
+export { RadixUIIcon };
