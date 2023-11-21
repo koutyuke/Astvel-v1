b67a0794158e8525b7d5c5fe290237ad6133fcfa
koutyuke <gunji.kousuke08@gmail.com>
2023-11-21 16:07:32 +0900

    feat: 🎈 (components/element/scrollArea) `horizontal`のスクロールバーの作成

--- src/components/elements/scrollArea/scrollArea.tsx
+++ src/components/elements/scrollArea/scrollArea.tsx
@@ -12,10 +12,17 @@ const ScrollArea: FC<Props> = ({ children, className, type }) => {
       <RadixScrollArea.Viewport className="h-full w-full">{children}</RadixScrollArea.Viewport>
       <RadixScrollArea.Scrollbar
         orientation="vertical"
-        className="box-border flex touch-none select-none border-l border-transparent p-0.5 transition-all duration-100 ease-in-out hover:border-gray-500 hover:bg-white/10 data-[orientation=vertical]:w-2.5 data-[orientation=vertical]:hover:w-4"
+        className="box-border flex touch-none select-none rounded-full border border-transparent p-0.5 transition-all duration-100 ease-in-out hover:m-0.5 hover:border-gray-500 hover:bg-white/10 data-[orientation=vertical]:w-2.5 data-[orientation=vertical]:hover:w-4"
+      >
+        <RadixScrollArea.Thumb className="relative flex-1 rounded-full bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+      </RadixScrollArea.Scrollbar>
+      <RadixScrollArea.Scrollbar
+        orientation="horizontal"
+        className="box-border flex touch-none select-none rounded-full border border-transparent p-0.5 transition-all duration-100 ease-in-out hover:m-0.5 hover:border-gray-500 hover:bg-white/10 data-[orientation=horizontal]:h-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:hover:h-4"
       >
         <RadixScrollArea.Thumb className="relative flex-1 rounded-full  bg-gray-500 before:absolute before:left-1/2 before:top-1/2 before:h-full before:min-h-[44px] before:w-full before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
       </RadixScrollArea.Scrollbar>
+      <RadixScrollArea.Corner className="bg-transparent" />
     </RadixScrollArea.Root>
   );
 };
