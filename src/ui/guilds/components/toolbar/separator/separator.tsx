e5fb34b8cd4c9216cf9301988ca2dc913434ba69
koutyuke <gunji.kousuke08@gmail.com>
2023-11-21 14:35:40 +0900

    style: 💄 (toolbar/separator) 高さを指定

--- src/ui/guilds/components/toolbar/separator/separator.tsx
+++ src/ui/guilds/components/toolbar/separator/separator.tsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 type Props = Pick<ComponentPropsWithoutRef<"div">, "className">;
 
 const Separator: FC<Props> = ({ className }) => {
-  return <div className={twMerge("mx-2 h-full w-[1px] rounded-full bg-gray-500", className)} />;
+  return <div className={twMerge("mx-2 h-10 w-[1px] rounded-full bg-gray-500", className)} />;
 };
 
 export { Separator };
