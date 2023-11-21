81a48bf9fd0c5c41cb51ba90db7d4667d108fe5c
koutyuke <gunji.kousuke08@gmail.com>
2023-11-21 16:10:12 +0900

    feat: 🎈 (ui/guilds/toolbar) overflow時にスクロールできるように変更

--- src/ui/guilds/components/toolbar/toolbar.tsx
+++ src/ui/guilds/components/toolbar/toolbar.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { twMerge } from "tailwind-merge";
+import { ScrollArea } from "components/elements/scrollArea";
 import { SelectedGuild } from "./guild";
 import { Separator } from "./separator";
 import { TravelerSize } from "./travelerSize";
@@ -12,16 +13,20 @@ type Props = {
   guildId: string;
 };
 const ToolBar: FC<Props> = ({ guildId }) => (
-  <div className="mx-8 flex h-14 items-center justify-start rounded-lg border border-gray-500 p-2 tablet:mx-16">
+  <div className="mx-8 flex h-14 items-center rounded-lg border border-gray-500 bg-black-2 px-2 tablet:mx-16">
     <SelectedGuild guildId={guildId} />
     <Separator />
-    <TravelerSize />
-    <Separator />
-    <div className={twMerge("scrollber-hidden flex  h-full w-0 flex-1 items-center space-x-2 overflow-x-auto px-2")}>
-      <Move guildId={guildId} />
-      <Reset />
-      <TeamSetting />
-    </div>
+    <ScrollArea className="h-fit w-[1px] flex-1" type="hover">
+      <div className="flex h-full w-full min-w-fit items-center space-x-2">
+        <TravelerSize />
+        <Separator />
+        <div className={twMerge(" flex w-[1px] min-w-fit flex-1 space-x-1")}>
+          <Move guildId={guildId} />
+          <Reset />
+          <TeamSetting />
+        </div>
+      </div>
+    </ScrollArea>
     <Separator />
     <BackToPage />
   </div>
