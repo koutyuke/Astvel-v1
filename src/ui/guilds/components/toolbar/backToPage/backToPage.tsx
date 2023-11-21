873e29305cec3eaf1599effc843b30924329de97
koutyuke <gunji.kousuke08@gmail.com>
2023-11-21 16:32:54 +0900

    feat: 🎈 (toolbar/backToPage) `close`ボタンの追加

--- src/ui/guilds/components/toolbar/backToPage/backToPage.tsx
+++ src/ui/guilds/components/toolbar/backToPage/backToPage.tsx
@@ -7,6 +7,7 @@ import { useResetTeam } from "stores/teams";
 import { useResetTeamTravelers, useResetUnselectedTravelers, useResetVoiceTravelers } from "stores/travelers";
 import { useResetChannels } from "stores/channels";
 import { twMerge } from "tailwind-merge";
+import { PlusIcon } from "components/icon/plus";
 
 type Props = Pick<ComponentPropsWithoutRef<"div">, "className">;
 
@@ -31,9 +32,11 @@ const BackToPage: FC<Props> = ({ className }) => {
 
   return (
     <AddDialog open={open} setOpen={setOpen}>
-      <div className="flex aspect-video w-96 max-w-[90vw]  flex-col items-center justify-between space-y-6 rounded-lg bg-black-1 p-6 text-white outline outline-1 outline-gray-500">
-        <p className="w-full text-center text-2xl">Back to Guild Select Page</p>
+      <div className="relative flex aspect-video w-96 max-w-[90vw] flex-col items-center justify-between gap-y-6 rounded-lg bg-black-1 p-6 text-white outline outline-1 outline-gray-500 tablet:px-10 tablet:py-6">
+        <p className="w-full text-center text-2xl">Return to Guilds</p>
         <p className="w-full text-center">
+          Return to the Guilds page.
+          <br />
           All teams will be deleted.
           <br />
           Are you sure?
@@ -62,6 +65,17 @@ const BackToPage: FC<Props> = ({ className }) => {
           >
             <p>OK</p>
           </BaseButton>
+        </div>
+        <div className="absolute right-6 top-6">
+          <button
+            type="button"
+            className="group rounded-full border border-gray-500 transition hover:border-green-500"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <PlusIcon size={24} className="rotate-45 stroke-gray-500 transition group-hover:stroke-green-500" />
+          </button>
         </div>
       </div>
     </AddDialog>
