49a630be598155dc56b57b13e25c25b061030676
koutyuke <gunji.kousuke08@gmail.com>
2023-11-22 12:30:23 +0900

    feat: 🎈 (toolbar/editTeam) `teamForm`, `ScrollArea`を使用

--- src/ui/guilds/components/toolbar/teamSetting/editTeam.tsx
+++ src/ui/guilds/components/toolbar/teamSetting/editTeam.tsx
@@ -1,104 +1,49 @@
 import { BaseButton } from "components/elements/button";
 import { FC, useState } from "react";
-import { Team } from "stores/teams";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import type { Team } from "stores/teams";
 import { EditIcon } from "components/icon/edit";
 import { TrashIcon } from "components/icon/trash";
 import { twMerge } from "tailwind-merge";
-import { tv } from "tailwind-variants";
-import { EmojiPickerSetting, TeamValue, schema } from "./store";
+import type { EmojiPickerSetting, TeamValue } from "./store";
+import { TeamEditor } from "./teamEditor";
 
 type Props = {
   setPickerSetting: (value: EmojiPickerSetting) => void;
-  teamDelete: (id: string) => void;
-  teamUpdate: (id: string, emoji: TeamValue["emoji"], name: TeamValue["name"]) => void;
+  deleteTeam: (id: string) => void;
+  updateTeam: (id: string, emoji: TeamValue["emoji"], name: TeamValue["name"]) => void;
   team: Team;
 };
 
-const variant = tv({
-  variants: {
-    form: {
-      view: "flex items-center ",
-      edit: "flex flex-col items-end space-y-2",
-      delete: "flex items-center ",
-    },
-    body: {
-      view: "flex",
-      edit: "grid w-full grid-cols-[auto_,_1fr] grid-rows-2 items-center gap-2",
-      delete: "flex",
-    },
-  },
-});
-
-const EditTeam: FC<Props> = ({ setPickerSetting, team, teamDelete, teamUpdate }) => {
+const EditTeam: FC<Props> = ({ setPickerSetting, team, deleteTeam, updateTeam }) => {
   const [mode, setMode] = useState<"view" | "edit" | "delete">("view");
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-    getValues,
-    reset,
-  } = useForm<TeamValue>({
-    defaultValues: {
-      emoji: team.iconEmoji,
-      name: team.name,
-    },
-    reValidateMode: "onChange",
-    resolver: zodResolver(schema),
-  });
 
+  if (mode === "edit") {
+    return (
+      <TeamEditor
+        setPickerSetting={setPickerSetting}
+        onSubmit={value => {
+          updateTeam(team.id, value.emoji, value.name);
+          setMode("view");
+        }}
+        onCancel={() => {
+          setMode("view");
+        }}
+        defaultValues={{
+          emoji: team.iconEmoji,
+          name: team.name,
+        }}
+        type="edit"
+      />
+    );
+  }
   return (
-    <form
-      className={twMerge("box-border rounded-lg border border-gray-500 p-2", variant({ form: mode }))}
-      onSubmit={handleSubmit(data => {
-        teamUpdate(team.id, data.emoji, data.name);
-      })}
-    >
-      <div className={twMerge("w-0 flex-1", variant({ body: mode }))}>
-        <label htmlFor="emoji" className={twMerge("justify-self-center px-1 text-sm", mode !== "edit" && "hidden")}>
-          Icon Emoji
-        </label>
-        <span className="flex items-end space-x-1">
-          <input
-            type="button"
-            id="emoji"
-            className={twMerge(
-              "h-8 w-8 rounded-full border-gray-500 text-center text-xl transition",
-              mode === "edit" && "border hover:border-green-500",
-            )}
-            onClick={() => {
-              setPickerSetting({
-                open: true,
-                func: value => {
-                  setValue("emoji", value.emoji);
-                },
-                currentEmoji: getValues("emoji"),
-              });
-            }}
-            disabled={mode !== "edit"}
-            {...register("emoji")}
-          />
-          {mode === "edit" && <span className="w-0 flex-1 text-sm text-gray-500">* Click and Choose!</span>}
-        </span>
-        <label htmlFor="name" className={twMerge("justify-self-center px-1 text-sm", mode !== "edit" && "hidden")}>
-          Team Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          disabled={mode !== "edit"}
-          className={twMerge(
-            "h-8 w-0 flex-1 truncate rounded-lg border-gray-500 px-3 transition ",
-            mode === "edit" && "w-full border hover:border-green-500",
-          )}
-          placeholder="Example: Team-A"
-          {...register("name")}
-        />
-      </div>
+    <div className={twMerge("box-border flex items-center gap-x-2 rounded-lg border border-gray-500 p-2")}>
+      <span className={twMerge("h-8 w-8 rounded-full border-gray-500 text-center text-2xl transition")}>
+        {team.iconEmoji}
+      </span>
+      <p className={twMerge("h-fit w-1 flex-1 truncate rounded-lg border-gray-500 transition ")}>{team.name}</p>
       {mode === "view" && (
-        <div className="flex space-x-2">
+        <>
           <BaseButton
             className="h-8 w-8 rounded-md text-sm"
             theme="normal"
@@ -121,41 +66,16 @@ const EditTeam: FC<Props> = ({ setPickerSetting, team, teamDelete, teamUpdate })
               <TrashIcon size={20} className="m-auto" />
             </span>
           </BaseButton>
-        </div>
-      )}
-      {mode === "edit" && (
-        <>
-          {(errors.emoji || errors.name) && (
-            <div className="w-full pl-2">
-              {errors.emoji && <p className="text-sm text-red-500 ">* {errors.emoji.message}</p>}
-              {errors.name && <p className="text-sm text-red-500">* {errors.name.message}</p>}
-            </div>
-          )}
-          <div className="flex space-x-2">
-            <BaseButton
-              theme="normal"
-              className="h-8 w-fit px-3 text-sm"
-              onClick={() => {
-                reset();
-                setMode("view");
-              }}
-            >
-              <span>Cancel</span>
-            </BaseButton>
-            <BaseButton theme="safety" className="h-8 w-fit px-3 text-sm" type="submit">
-              <span>Update</span>
-            </BaseButton>
-          </div>
         </>
       )}
       {mode === "delete" && (
-        <div className="flex space-x-2">
+        <>
           <BaseButton
             theme="danger"
             className="h-8 w-fit px-3 text-sm"
             type="submit"
             onClick={() => {
-              teamDelete(team.id);
+              deleteTeam(team.id);
               setMode("view");
             }}
           >
@@ -165,15 +85,14 @@ const EditTeam: FC<Props> = ({ setPickerSetting, team, teamDelete, teamUpdate })
             theme="normal"
             className="h-8 w-fit px-3 text-sm"
             onClick={() => {
-              reset();
               setMode("view");
             }}
           >
             <span>Cancel</span>
           </BaseButton>
-        </div>
+        </>
       )}
-    </form>
+    </div>
   );
 };
 
