5a169d31200f31fad90630fe1c68e3c2e8c4be5d
koutyuke <gunji.kousuke08@gmail.com>
2023-11-22 17:57:36 +0900

    typo: 🖋️ (dialog) typo修正

--- src/components/elements/dialog/dialog.tsx
+++ src/components/elements/dialog/dialog.tsx
@@ -7,10 +7,10 @@ type Props = {
   setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const createDialog: (TrigerButton: ReactNode) => FC<Props> = TrigerButton => {
+const createDialog: (TriggerButton: ReactNode) => FC<Props> = TriggerButton => {
   const Dialog: FC<Props> = ({ children, open, setOpen }) => (
     <radixDialog.Root open={open} onOpenChange={setOpen}>
-      <radixDialog.Trigger asChild>{TrigerButton}</radixDialog.Trigger>
+      <radixDialog.Trigger asChild>{TriggerButton}</radixDialog.Trigger>
       <radixDialog.Portal>
         <radixDialog.Overlay className="fixed inset-0 z-[999] bg-[rgba(0,0,0,0.5)]" />
         <radixDialog.Content className="fixed left-[50%] top-[50%] z-[1000] translate-x-[-50%] translate-y-[-50%]">
