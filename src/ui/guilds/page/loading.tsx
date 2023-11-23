48619f64a546f81ccc9b4a14bc301c5db0ffead1
koutyuke <gunji.kousuke08@gmail.com>
2023-11-23 14:22:06 +0900

    feat: 🎈 (ui/guilds/loading) 余計な子要素を削除

--- src/ui/guilds/page/loading.tsx
+++ src/ui/guilds/page/loading.tsx
@@ -15,46 +15,20 @@ const Loading: FC = () => {
         <Separator />
         <div className="box-border h-9 w-9  rounded-md border border-gray-500 bg-black-3" />
       </div>
-      {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-      <div className="scrollber-hidden flex grow snap-x snap-mandatory space-x-4 overflow-x-auto px-8 tablet:px-16">
+      <div className="flex grow snap-x snap-mandatory space-x-4 overflow-x-auto px-8 scrollbar-hidden tablet:px-16">
         <div className="w-1/3 min-w-[15rem] snap-center  space-y-4 overflow-hidden rounded-lg border border-gray-500 bg-black-2 p-2.5">
           <div className="box-border w-full items-center space-y-3 rounded-lg border border-gray-500 bg-black-2 p-3">
-            <div className="h-5 w-full space-y-2 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border w-full items-center space-y-3 rounded-lg border border-gray-500 bg-black-2 p-3">
-              <div className="h-5 w-full space-y-2 rounded-lg border border-gray-500 bg-black-3" />
-              <div className="box-border h-32 w-full space-y-4 rounded-lg border border-gray-500 p-2" />
-            </div>
+            <div className="h-6 w-full space-y-2 rounded-lg border border-gray-500 bg-black-3" />
+            <div className="box-border h-48 w-full items-center space-y-3 rounded-lg border border-gray-500 bg-black-2 p-3" />
           </div>
         </div>
         <div className=" w-1/3 min-w-[15rem] snap-center  space-y-4 overflow-hidden rounded-lg border border-gray-500 bg-black-2 p-2.5">
-          <div className="grid w-full grid-cols-[repeat(auto-fill_,_minmax(5rem_,_1fr))] justify-items-center gap-3">
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-          </div>
+          <div className="grid w-full grid-cols-[repeat(auto-fill_,_minmax(5rem_,_1fr))] justify-items-center gap-3" />
         </div>
         <div className="w-1/3 min-w-[15rem] snap-center space-y-2 overflow-hidden rounded-lg border border-gray-500 bg-black-2 p-2.5">
           <div className="box-border w-full items-center space-y-3 rounded-lg border border-gray-500 bg-black-2 p-3">
-            <div className="h-5 w-full space-y-2 rounded-lg border border-gray-500 bg-black-3" />
-            <div className="box-border w-full space-y-4 rounded-lg border border-gray-500 p-3">
-              <div className="grid w-full grid-cols-[repeat(auto-fill_,_minmax(5rem_,_1fr))] justify-items-center gap-3">
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-                <div className="box-border h-24 w-20 rounded-lg border border-gray-500 bg-black-3" />
-              </div>
-            </div>
+            <div className="h-6 w-full space-y-2 rounded-lg border border-gray-500 bg-black-3" />
+            <div className="box-border h-48 w-full space-y-4 rounded-lg border border-gray-500 p-3" />
           </div>
         </div>
       </div>
