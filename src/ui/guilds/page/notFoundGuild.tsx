f58cfcace11aae2d7b1189f09431d3ad2bffa614
koutyuke <gunji.kousuke08@gmail.com>
2023-11-23 14:22:47 +0900

    feat: 🎈 (ui/guilds/notFoundGuild) 招待URLのテキストを少なくした

--- src/ui/guilds/page/notFoundGuild.tsx
+++ src/ui/guilds/page/notFoundGuild.tsx
@@ -4,8 +4,8 @@ import { ExternalLink } from "components/elements/link";
 import { botInviteUrl } from "stores/url/bot";
 
 const NotFoundGuild: FC = () => (
-  <div className="grid grid-rows-[3rem_,_8rem_,_1fr] items-center justify-items-center gap-4 tablet:grid-cols-[8rem_,_18rem] tablet:grid-rows-[3rem_,_1fr]">
-    <p className="text-4xl">No Guild</p>
+  <div className="grid grid-rows-[3rem_,_8rem_,_1fr] items-center justify-items-center gap-2 tablet:grid-cols-[8rem_,_18rem] tablet:grid-rows-[3rem_,_1fr]">
+    <p className="text-4xl font-semibold">No Guild</p>
     <div className="h-fit w-fit tablet:col-span-1 tablet:row-[1_/_3]">
       <Image
         src="/space/crashed-rocket.png"
@@ -18,13 +18,12 @@ const NotFoundGuild: FC = () => (
     </div>
     <div className="flex flex-col items-center justify-center space-y-2 text-center">
       <p>Guild doesn&apos;t exist or has no bots in it.</p>
-      <p className="leading-6">
-        Please check the URL or
-        <br />
-        <ExternalLink url={botInviteUrl} icon className="border-b border-white hover:border-green-500">
-          invite the bot to join your guild.
+      <span>Please check the URL or</span>
+      <span>
+        <ExternalLink url={botInviteUrl} icon className="w-fit border-b  border-white hover:border-green-500">
+          invite the bot
         </ExternalLink>
-      </p>
+      </span>
     </div>
   </div>
 );
